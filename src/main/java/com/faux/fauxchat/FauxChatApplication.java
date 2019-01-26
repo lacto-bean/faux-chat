b301e83d3c859816fb2e489375a7277f4a899ada
sabarish_duke <sabarishduke@gmail.com>
2019-01-26 15:52:43 +0530

    initialization

---
+++ src/main/java/com/faux/fauxchat/FauxChatApplication.java
@@ -0,0 +1,14 @@
+package com.faux.fauxchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FauxChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FauxChatApplication.class, args);
+	}
+
+}
+
